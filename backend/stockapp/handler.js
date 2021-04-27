0150dff265f0cf36761670a3473d8e3d14b8c379
Eileen <eileenzhou2012@gmail.com>
2021-04-27 12:56:55 -0400

    updates

---
+++ backend/stockapp/handler.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  if (event.path === '/whoami' && event.httpMethod === 'GET') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({username: 'eileenzhou_'})
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
